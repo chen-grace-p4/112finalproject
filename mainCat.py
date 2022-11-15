def4d35f3f8f8c319a03a9f15341cc94761a73b8
Grace Chen <gchen20@stuy.edu>
2022-11-15 01:31:29 -0500

    added directoinal still sprites

--- mainCat.py
+++ mainCat.py
@@ -18,6 +18,11 @@ class Cat():
     
     def appStarted(self, app):
         app.catImage = app.loadImage(self.image)
+        spritestrip = app.loadImage('stillcatspritesheet.png')
+        app.stillSprites = []
+        for i in range(4):
+            sprite = spritestrip.crop((64*i, 0, (64*i)+64, 64))
+            app.stillSprites.append(sprite)
     
     def makeCatVisible(self, app):
         if (self.cx < (self.scrollX + self.scrollMargin)):
@@ -38,12 +43,16 @@ class Cat():
     def keyPressed(self, app, event):
         if (self.toggleMoveLeft and event.key == "Left"):
             self.moveCat(app, -10, 0)
+            app.catImage = app.stillSprites[1]
         elif (self.toggleMoveRight and event.key == "Right"):
             self.moveCat(app, +10, 0)
+            app.catImage = app.stillSprites[0]
         elif (self.toggleMoveUp and event.key == "Up"):
             self.moveCat(app, 0, -10)
+            app.catImage = app.stillSprites[3]
         elif (self.toggleMoveDown and event.key == "Down"):
             self.moveCat(app, 0, +10)
+            app.catImage = app.stillSprites[2]
 
     def redrawAll(self, app, canvas):
         x = self.cx
